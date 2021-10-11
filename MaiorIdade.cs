77e43a4d41a86488db141cbdce9b04961da7570f
Josias <87546094+Josyas@users.noreply.github.com>
2021-10-11 11:08:44 -0300

    Add files via upload

---
+++ MaiorIdade.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace maior_idade_POO {
+    class MaiorIdade {
+
+        private string nome;
+
+        public string Nome {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        private int idade;
+        public int Idade {
+            get { return idade; }
+            set {
+                if (value > 0) {
+                    idade = value;
+                }
+                else {
+                    this.idade = 0;
+                }
+            }
+        }
+
+        public MaiorIdade() {
+            this.Nome = "";
+            this.Idade = 0;
+        }
+
+        public MaiorIdade(string nome, int idade) {
+            this.nome = nome;
+            this.idade = idade;
+        }
+        public void ExibirDados() {
+            Console.WriteLine("Nome: "+ this.Nome);
+            Console.WriteLine("Idade: "+ this.Idade);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace maior_idade_POO {
+    class Program {
+        static void Main(string[] args) {
+
+            MaiorIdade pessoa1, pessoa2, pessoa3;
+            pessoa1 = new MaiorIdade();
+            pessoa2 = new MaiorIdade();
+            
+
+            Console.WriteLine("Dados da pimeira pessoa!");
+            Console.Write("Nome: ");
+            pessoa1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da segunda pessoa!");
+            Console.Write("Nome: ");
+            pessoa2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da terceira pessoa!");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            pessoa3 = new MaiorIdade(nome, idade);
+
+            if (pessoa1.Idade > pessoa2.Idade || pessoa1.Idade > pessoa3.Idade) {
+                Console.WriteLine("Pessoa com maior idade é: ");
+                pessoa1.ExibirDados();
+                
+            }
+            else {
+                if (pessoa2.Idade > pessoa1.Idade || pessoa2.Idade > pessoa3.Idade) {
+                    Console.WriteLine("Pessoa com maior idade é: ");
+                    pessoa2.ExibirDados();
+                }
+                else {
+                    if (pessoa3.Idade > pessoa1.Idade || pessoa3.Idade > pessoa2.Idade) {
+                        Console.WriteLine("Pessoa com maior idade é: ");
+                        pessoa3.ExibirDados();
+                    }
+                    else {
+                        Console.WriteLine("Pessoa com a mesma idade!");
+                    }
+                }
+            }
+
+
+            
+        }
+    }
+}
